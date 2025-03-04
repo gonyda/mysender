aa0bb8a440dce749d54961aca589ff72ebd6370f
gony <bbsk3939@gmail.com>
2025-03-04 21:53:20 +0900

    미사용 properties 삭제

--- src/main/java/org/bbsk/mysender/MysenderApplication.java
+++ src/main/java/org/bbsk/mysender/MysenderApplication.java
@@ -1,13 +1,10 @@
 package org.bbsk.mysender;
 
-import org.bbsk.mysender.properties.GlobalProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling // 스케줄링
-@EnableConfigurationProperties(GlobalProperties.class)
 @SpringBootApplication
 public class MysenderApplication {
 
