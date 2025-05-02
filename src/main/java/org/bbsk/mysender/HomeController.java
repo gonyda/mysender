45b9a31c6fe5315e4d7a0922d69c3d8c1cd16dee
gony <bbsk3939@gmail.com>
2025-05-02 22:24:37 +0900

    외환거래 화면 테스트

--- src/main/java/org/bbsk/mysender/HomeController.java
+++ src/main/java/org/bbsk/mysender/HomeController.java
@@ -1,17 +1,10 @@
 package org.bbsk.mysender;
 
-import org.bbsk.mysender.fmkorea.scheduler.FmKoreaScheduler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    private final FmKoreaScheduler fmKoreaScheduler;
-
-    public HomeController(FmKoreaScheduler fmKoreaScheduler) {
-        this.fmKoreaScheduler = fmKoreaScheduler;
-    }
 
     @GetMapping("/")
     public String home() {
@@ -24,7 +17,7 @@ public class HomeController {
     }
 
     @GetMapping("/test")
-    public void test() {
-        fmKoreaScheduler.getFmKoreaCrawlingByPopularToStock();
+    public String test() {
+        return "test";
     }
 }
